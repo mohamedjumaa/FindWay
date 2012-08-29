87d2838ccd8efa6593891d67bbc45503d4b3c489
Roger Liu <User@Rogers-Mac.local>
2012-08-29 21:14:19 +0800

    Add Map tab, Message Tab and other tab.

---
+++ FindWay/MessageTab.h
@@ -0,0 +1,14 @@
+//
+//  MessageTab.h
+//  FindWay
+//
+//  Created by Roger Liu on 12/8/29.
+//  Copyright (c) 2012年 Lion User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessageTab : UITableViewController
+<UITableViewDataSource>
+
+@end
